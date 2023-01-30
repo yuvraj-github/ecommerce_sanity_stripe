654562d54f5950e927064c1c583a4b423977f566
yuvraj <yuvi.unique@gmail.com>
2023-01-30 21:20:41 +0530

    Added some codes in the layout and footer section

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ import { Product, FooterBanner, HeroBanner } from '../components'
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-      {console.log(bannerData)}
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />     
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        { products?.map(
-          (product) => product.name)}
+        { products?.map((product) => <Product key={product._id} product={product}/>)}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={ bannerData && bannerData[0]} />
     </>
   )
 }
