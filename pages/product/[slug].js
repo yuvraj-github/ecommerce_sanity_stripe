802561a5b8979ab08381ec334c0e26f53897cc49
yuvraj <yuvi.unique@gmail.com>
2023-01-31 13:42:39 +0530

    Added functionality of product dynamic images

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { client, urlFor } from '../../lib/client';
 import { Product } from '@/components';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
+    const [index, setIndex] = useState(0);
     return (
         <div>
             <div className='product-detail-container'>
                 <div>
                     <div className='image-container'>
-                        <img src={urlFor(image && image[0])} />
+                        <img src={urlFor(image && image[index])} className='product-detail-image' />
                     </div>
-                    {/* <div className='small-images-container' >
+                    <div className='small-images-container' >
                         {image?.map((item, i) => (
-                            <img src={ urlFor(item) } className='' onMouseEnter='' />
+                            <img src={urlFor(item)}
+                                className={i === index ? 'small-image selected-image' : 'small-image'}
+                                onMouseEnter={() => setIndex(i)}
+                            />
                         ))}
-                    </div> */}
+                    </div>
                 </div>
                 <div className='product-detail-desc'>
                     <h1>{name}</h1>
@@ -52,7 +56,7 @@ const ProductDetails = ({ product, products }) => {
                 <div className='marquee'>
                     <div className='maylike-products-container track'>
                         {products.map((item) => (
-                            <Product key={item._id} product={item}/>
+                            <Product key={item._id} product={item} />
                         ))}
                     </div>
                 </div>
